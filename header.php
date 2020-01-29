4f3eb6a21467631327d419f0e37417e7ce1f8d58
Faycalth <faycalthamri38@gmail.com>
2020-01-29 20:17:05 +0100

    modif images

--- header.php
+++ header.php
@@ -1,8 +1,33 @@
-<?php 
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : Skeleton 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20130902
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" />
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<div id="page" class="container">
+
 <div id="header">
 		<div id="logo">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/pic02.jpg" alt="">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/pic02.png" alt="">
 			<h1><a href="#">Fayçal THAMRI</a></h1>
 		</div>
 		<div id="menu">
